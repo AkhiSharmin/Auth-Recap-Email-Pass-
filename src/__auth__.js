ac01f7a73f7a1d6fc0498278cf811c5bea49f4d3
AkhiSharmin <akhisharmin100@gmail.com>
2025-04-20 19:29:59 +0600

    first commit

---
+++ src/__auth__.js
@@ -0,0 +1,4 @@
+/**
+ * 1. login === sing in ==> u are an old user. you already have the credential (email and pass) to login
+ * 2. register === sing up === create account ===> you    are a brand new user. you have never used the website before you do not have any email and pass that have access
+ */
