cbaa1a951a74a5f52ea6be1ca43b6c7754ddfb07
AkhiSharmin <akhisharmin100@gmail.com>
2025-04-23 22:27:13 +0600

    some ofupdate

--- src/components/SingUp/SingUp.jsx
+++ src/components/SingUp/SingUp.jsx
@@ -1,10 +1,12 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase.init";
+import { FaEye } from "react-icons/fa";
 
 const SingUp = () => {
   const [success, setSuccess] = useState(false);
   const [errorMassage, setErrorMassage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
   const handelSingUp = (e) => {
     e.preventDefault();
@@ -48,7 +50,7 @@ const SingUp = () => {
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
       <h3 className="text-3xl ml-4 font-bold">Sing Up now!</h3>
       <form onSubmit={handelSingUp} className="card-body">
-        <div className="form-control">
+        <div className="form-control relative">
           <label className="label">Email</label>
           <input
             name="email"
@@ -61,14 +63,19 @@ const SingUp = () => {
         <div className="form-control">
           <label className="label">Password</label>
           <input
+            type={showPassword ? "text" : "password"}
             name="password"
-            type="password"
             className="input my-4"
             placeholder="Password"
             required
           />
         </div>
-        <button className="btn btn-xs">e</button>
+        <button
+          onClick={() => setShowPassword(!showPassword)}
+          className="btn btn-xs absolute right-[41px] top-[205px]"
+        >
+          <FaEye></FaEye>
+        </button>
         <div>
           <a className="link link-hover">Forgot password?</a>
         </div>
